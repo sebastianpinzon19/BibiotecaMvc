02959eb501018403276d1bb787c24691a7bd5e62
sebastian pinzon reyes <152411485+sebastianpinzon19@users.noreply.github.com>
2024-08-13 23:41:56 -0500

    repositorio
    repositorio de BibliotecaMvc

---
+++ bibliotecaWebApplicationMvc/Models/Autorlibro.cs
@@ -0,0 +1,14 @@
+﻿namespace BibliotecaWebApplicationMvc.Models
+{
+    public class AutorLibro
+    {
+        public Guid AutorId { get; set; }
+        public Autor Autor { get; set; }
+
+        public Guid LibroId { get; set; }
+        public Libro Libro { get; set; }
+
+
+
+    }
+}
